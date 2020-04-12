26d23eb0da838c9a8a203ea904beddd556008c74
Innocent Zenda <zendainnocent@gmail.com>
2020-04-12 10:16:36 +0300

    feat: add absolute url for product

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class ProductManager(models.Manager):
@@ -19,6 +20,9 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('main:product-detail', kwargs={'pk': self.pk})
 
 
 class ProductImage(models.Model):
