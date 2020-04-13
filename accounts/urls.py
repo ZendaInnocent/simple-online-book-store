34a9f5f5b37e90923a5a9098a2a29d60d48777dd
Innocent Zenda <zendainnocent@gmail.com>
2020-04-13 17:06:09 +0300

    feat: add page for showing user addresses

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,5 @@ app_name = 'accounts'
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('signup/', views.UserRegistrationView.as_view(), name='signup'),
+    path('addresses/', views.AddressListView.as_view(), name='address-list'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 
 from accounts import forms
+from accounts.models import Address
 
 
 class UserRegistrationView(CreateView):
     form_class = forms.UserCreationForm
     template_name = 'registration/signup.html'
     success_url = '/'
+
+
+class AddressListView(ListView):
+    model = Address
+    template_name = 'accounts/address_list.html'
+
+    def get_queryset(self):
+        return self.model.objects.filter(user=self.request.user)
