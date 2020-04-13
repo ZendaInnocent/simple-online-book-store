7d3d033adfeca671d91157315a1aad4aacf549cb
Innocent Zenda <zendainnocent@gmail.com>
2020-04-13 13:11:36 +0300

    refactor: move products url test (main -> products)

--- products/tests/test_urls.py
+++ products/tests/test_urls.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
 
-# Create your tests here.
+from products import views
+
+
+class TestProductsAppUrls(TestCase):
+
+    def test_product_list_url_resolves(self):
+        product_list_url = reverse('products:product-list')
+        self.assertEqual(resolve(product_list_url).func.view_class, views.ProductListView)
+
+    
+    def test_product_detail_url_resolves(self):
+        product_detail_url = reverse('products:product-detail', kwargs={'pk': 1})
+        self.assertEqual(resolve(product_detail_url).func.view_class, views.ProductDetailView)
