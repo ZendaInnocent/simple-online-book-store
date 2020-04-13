e148499ed5365e7b051fd190430d6674a18761cb
Innocent Zenda <zendainnocent@gmail.com>
2020-04-13 11:38:23 +0300

    feat: add user registration page

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
 
-# Create your views here.
+from accounts import forms
+
+
+class UserRegistrationView(CreateView):
+    form_class = forms.UserCreationForm
+    template_name = 'registration/signup.html'
+    success_url = '/'
