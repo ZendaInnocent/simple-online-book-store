ad258974f55cb2b324885b4c7ab39b80c8ff33b9
Innocent Zenda <zendainnocent@gmail.com>
2020-04-15 22:44:55 +0300

    feat: add functionality to remove item from cart.

--- cart/urls.py
+++ cart/urls.py
@@ -5,5 +5,6 @@ app_name = 'cart'
 
 urlpatterns = [
   path('', views.cart_view, name='order-summary'),
-  path('add-to-cart/<slug:slug>/', views.add_to_cart_view, name='add-to-cart'),
+  path('add-to-cart/<slug:slug>/', views.add_item_to_cart, name='add-to-cart'),
+  path('remove-from-cart/<slug:slug>/', views.remove_item_from_cart, name='remove-from-cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib import messages
 from django.shortcuts import redirect, get_object_or_404
 from django.urls import reverse
+from django.http import HttpResponse
 
 from cart.models import Cart, Order
 from products.models import Product
@@ -23,7 +24,7 @@ def cart_view(request):
     return redirect("products:product-list")
 
 
-def add_to_cart_view(request, slug):
+def add_item_to_cart(request, slug):
   item = get_object_or_404(Product, slug=slug)
   order_item, created = Cart.objects.get_or_create(
     item=item,
@@ -43,10 +44,45 @@ def add_to_cart_view(request, slug):
     else:
       order.order_items.add(order_item)
       messages.info(request, "This item was added to your cart.")
-      return redirect('products:product-detail', kwargs={'slug':slug})
+      return redirect(reverse('products:product-detail', kwargs={'slug':slug}))
     
   else:
     order = Order.objects.create(user=request.user)
     order.order_items.add(order_item)
     messages.info(request, "This item was added to your cart.")
     return redirect('product:product-detail', kwargs={'slug':slug})
+
+
+def remove_item_from_cart(request, slug):
+  item = get_object_or_404(Product, slug=slug)
+  cart_qs = Cart.objects.filter(user=request.user, item=item)
+
+  if cart_qs.exists():
+    cart = cart_qs[0]
+    # Checking the cart quantity
+    if cart.quantity >= 1:
+        cart.delete()
+
+  order_qs = Order.objects.filter(
+    user=request.user,
+    ordered=False
+  )
+
+  if order_qs.exists():
+    order = order_qs[0]
+    # check if the order item is in the order
+    if order.order_items.filter(item__slug=item.slug).exists():
+      order_item = Cart.objects.filter(
+          item=item,
+          user=request.user,
+      )[0]
+      order.order_items.remove(order_item)
+      order.order_items = 0
+      messages.info(request, "This item was removed from your cart.")
+      return redirect(reverse('products:product-detail', kwargs={'slug':slug}))
+    else:
+      messages.info(request, "This item was removed from your cart")
+    return redirect(reverse('products:product-detail', kwargs={'slug':slug}))
+  else:
+    messages.info(request, "You do not have an active order")
+    return redirect(reverse('products:product-detail', kwargs={'slug':slug}))
