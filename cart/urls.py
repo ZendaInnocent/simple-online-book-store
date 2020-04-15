e289c58e3d08d1c80c098ae8340f8ea56152186b
Innocent Zenda <zendainnocent@gmail.com>
2020-04-15 19:17:29 +0300

    feat: add view cart functionality

--- cart/urls.py
+++ cart/urls.py
@@ -1,7 +1,8 @@
-from django import urls
+from django.urls import path
+from . import views
 
 app_name = 'cart'
 
 urlpatterns = [
-  
+  path('', views.cart_view, name='order-summary'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect, get_object_or_404
 
-# Create your views here.
+from .models import Cart, Order
+
+
+def cart_view(request):
+
+  user = request.user
+
+  carts = Cart.objects.filter(user=user)
+  orders = Order.objects.filter(user=user, ordered=False)
+
+  if carts.exists():
+    order = orders[0]
+    return render(request, 'cart/order_summary.html', {"carts": carts, 'order': order})
+  
+  else:
+    messages.warning(request, "You do not have an active order")
+    return redirect("products:product-list")
+
